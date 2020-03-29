c945c1f2c90e6d8984363933795d9be88399cd77
ashish336b <ashish336b@gmail.com>
2020-03-29 10:15:45 +0545

    seeder for admin table

---
+++ database/seeds/AdminSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $password = '11111111';
+        DB::table("admins")->insert([
+            [
+                'name' => 'Shrijan Bir Malakar',
+                'email' => 'shrijan@gmail.com',
+                'job_title' => 'admin',
+                'password' => Hash::make($password)
+            ],
+            [
+                'name' => 'Ashish Bhandari',
+                'email' => 'admin@gmail.com',
+                'job_title' => 'admin',
+                'password' => Hash::make($password)
+            ],
+        ]);
+    }
+}
