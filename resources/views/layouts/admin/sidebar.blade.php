ebb0a9dc1b6657581daded4699f63a4ac4d29f61
ashish336b <ashish336b@gmail.com>
2020-04-01 23:25:11 +0545

    added active classes for visited url

--- resources/views/layouts/admin/sidebar.blade.php
+++ resources/views/layouts/admin/sidebar.blade.php
@@ -4,20 +4,19 @@
             <div class="logo"><a href="{{ url('/') }}/admin"><span>Admin Dashboard</span></a></div>
             <ul>
                 <li class="label">Student</li>
-                <li><a href="{{ route('admin.teachers') }}"><i class="ti-calendar"></i> Teacher </a>
+                <li class="{{Route::is('admin.teachers')?'active': ''}}"><a href="{{ route('admin.teachers') }}"><i class="ti-calendar"></i> Teacher </a>
                 </li>
-                <li><a href="{{ route('admin.students') }}"><i class="ti-calendar"></i> Student </a>
+                <li class="{{Route::is('admin.students')?'active': ''}}"><a href="{{ route('admin.students') }}"><i class="ti-calendar"></i> Student </a>
                 </li>
                 <li class="label">Department and classes</li>
-                <li><a href="{{ route('admin.classroom') }}"><i class="ti-calendar"></i>Classes</a>
+                <li class="{{Route::is('admin.classroom')?'active': ''}}"><a href="{{ route('admin.classroom') }}"><i class="ti-calendar"></i>Classes</a>
                 </li>
-                <li><a href="{{ route('admin.department') }}"><i
+                <li class="{{Route::is('admin.department')?'active': ''}}"><a href="{{ route('admin.department') }}"><i
                             class="ti-calendar"></i>Department</a></li>
-                <li class="label">Notification and Message</li>
+                <li class="label">Information</li>
                 {{-- <li><a href="{{ route('admin.message') }}"><i class="ti-calendar"></i>Message</a> --}}
                 </li>
-                <li><a href="{{ route('admin.notification') }}"><i class="ti-calendar"></i>Send
-                        Notification</a></li>
+                <li class="{{Route::is('admin.notification')?'active': ''}}"><a href="{{ route('admin.notification') }}"><i class="ti-calendar"></i>Notice</a></li>
             </ul>
         </div>
     </div>
