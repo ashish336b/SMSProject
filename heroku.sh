a1942a52c7ad8f2447e4bec95097f0b62c718e3a
ashish336b <ashish336b@gmail.com>
2020-04-03 22:50:29 +0545

    setting environment variable to heroku server using heroku.sh

--- heroku.sh
+++ heroku.sh
@@ -1,5 +1,10 @@
+# database
 heroku config:add DB_HOST=ec2-34-193-232-231.compute-1.amazonaws.com
 heroku config:add DB_PORT=5432
 heroku config:add DB_DATABASE=dbit71n4mioq7q
 heroku config:add DB_USERNAME=urwbdxaikemcmn
 heroku config:add DB_PASSWORD=63f5d0a85dc84e87c6ba06833ab9a32bdc21bd0845be5553596a6694822b3bdb
+# paypal configuratoin
+heroku config:add CLIENT_ID=AeEwKTEsfKC5y7pxN5mJVOVL22xzEtIQfGQwGR_clWktSJfJoqjxGEygBMfKFvCtJcx7F27EO61GweLb
+heroku config:add CLIENT_SECRET=EIuQDgRENQfwFHvl6NT2BoZjJ2U7ZGQAjiSY1Oh3bIPI7ItOhNEgXRTXkxpB6xBYbBi4aodK9Abx3g9f
+
