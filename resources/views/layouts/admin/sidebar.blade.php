446db3b8e721c1b5ffc6d1d6237ccdd027fcd2af
ashish336b <ashish336b@gmail.com>
2020-04-10 17:01:00 +0545

    dashboard icons

--- resources/views/layouts/admin/sidebar.blade.php
+++ resources/views/layouts/admin/sidebar.blade.php
@@ -11,34 +11,34 @@
                 <li class="label">Students and Teacher</li>
                 <li
                     class="{{ Route::is('admin.teachers')?'active': '' }}">
-                    <a href="{{ route('admin.teachers') }}"><i class="ti-calendar"></i> Teacher </a>
+                    <a href="{{ route('admin.teachers') }}"><i class="ti-user"></i> Teacher </a>
                 </li>
                 <li
                     class="{{ Route::is('admin.students')?'active': '' }}">
-                    <a href="{{ route('admin.students') }}"><i class="ti-calendar"></i> Student </a>
+                    <a href="{{ route('admin.students') }}"><i class="ti-user"></i> Student </a>
                 </li>
                 <li class="label">Department and classes</li>
                 <li
                     class="{{ Route::is('admin.classroom')?'active': '' }}">
-                    <a href="{{ route('admin.classroom') }}"><i class="ti-calendar"></i>Classes</a>
+                    <a href="{{ route('admin.classroom') }}"><i class="ti-bag"></i>Classes</a>
                 </li>
                 <li
                     class="{{ Route::is('admin.department')?'active': '' }}">
                     <a href="{{ route('admin.department') }}"><i
-                            class="ti-calendar"></i>Department</a></li>
+                            class="ti-panel"></i>Department</a></li>
                 <li class="label">Information</li>
                 <li
                     class="{{ Route::is('admin.students.feePaymentList')? 'active': '' }}">
                     <a href="{{ route('admin.students.feePaymentList') }}"><i
-                            class="ti-calendar"></i>Fee Information</a>
+                            class="ti-angle-double-right"></i>Fee Information</a>
                 </li>
                 <li
                     class="{{ Route::is('admin.notice')?'active': '' }}">
-                    <a href="{{ route('admin.notice') }}"><i class="ti-calendar"></i>Notice</a></li>
+                    <a href="{{ route('admin.notice') }}"><i class="ti-dashboard"></i>Notice</a></li>
                 <li class="label">Admin</li>
                 <li
                     class="{{ Route::is('admin.list')?'active': '' }}">
-                    <a href="{{ route('admin.list') }}"><i class="ti-calendar"></i>Admins List</a>
+                    <a href="{{ route('admin.list') }}"><i class="ti-agenda"></i>Admins List</a>
                 </li>
             </ul>
         </div>
